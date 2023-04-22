98558e4d4aa15008125b5def22ff12d11bc43785
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-04-22 15:46:03 +0100

    created the header file

---
+++ expert.h
@@ -0,0 +1,20 @@
+#ifndef _expert_
+#define _expert_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+
+void main_menu(void);
+void Banquet(void);
+void Wedding(void);
+void Birthday(void);
+int Budget(void);
+int Season(void);
+void About(void);
+void Corporate();
+float GrandTotal(float cost_1, float cost_2, float cost_3, float cost_4);
+
+
+#endif
